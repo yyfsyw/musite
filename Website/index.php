a0f3df05d4ec3ec4d149036bdfa3a0236279d85e
ylskt3 <ylskt3@mail.missouri.edu>
2016-06-21 11:20:13 -0500

    web updated

--- Website/index.php
+++ Website/index.php
@@ -9,6 +9,20 @@
 		fclose($myfile);
 		$output = shell_exec("java -jar musitePractice.jar $selection");
 		//echo "$output";
+		$to = $_POST['emailAddress'];
+		//echo "$to";
+		$subject = "Musite Result";
+		//$txt = "Hello world!";
+		$headers = "From: ylskt3@mail.missouri.edu";
+
+		if(mail($to, $subject, $output, $headers))
+			echo "success";
+		else
+			echo "error";
+			echo $to;
+			echo $subject;
+			//echo $output;
+			echo $headers;
 	}
 ?>
 
@@ -45,10 +59,28 @@
 
 			<legend id="mainLegend">SUBMISSION:</legend>
 
+			<fieldset id="accessions">
+			<legend>Accessions:</legend>
+				<div class="inset">Input comma-separated accessions (limit of 100). <a id="uniprot_example" class="theme" href="files/uniprot_example.txt" target="_blank">example</a><br/>
+				<select name="accession_type" id="accession_type" size="1">
+				<option value="uniprot">UniProt</option>
+				</select><strong>:</strong>
+				
+				<input type="text" name="accession_text" id="accession_text" size="40" />
+				</div>
+			</fieldset>
+
 			<fieldset>
 			<legend>Sequence:</legend>
 			<div class="inset">Input protein sequence(s) in FASTA format (limit of 100). <a id="fasta_example" class="theme" href="files/fasta_example.txt" target="_blank">example</a><br/>
-			<textarea name="inputSeq" id="sequence" class="resizable" rows="12" cols="60"></textarea></div><br>
+			<textarea name="inputSeq" id="sequence" class="resizable" rows="12" cols="100"></textarea></div><br>
+
+			<legend>Upload Sequence file:</legend>
+			<div class="inset">Input protein sequence(s) file in FASTA format (limit of 100). <a id="fasta_example" class="theme" href="files/fasta_example.txt" target="_blank">example</a><br/>
+			<br>Your email: <input type="email" name="emailAddress" id="accession_text" size="39" /><br>
+			<br>
+			<label for="c">Upload File: </label>
+			<input id="c" name="uploadedFile" type="file" aria-required="true"></div><br>
 
 			<legend>Selections:</legend><br>
 			<div class="inset">
@@ -70,19 +102,31 @@
 				<br>
 				<br>
 			</fieldset>
+			</form>
 
-			</fieldset>
 			<fieldset>
 			<legend id="mainLegend">RESULT:</legend>
 			<fieldset>
-			<textarea id="sequence" class="resizable" rows="25" cols="60"><?php echo "$output"; ?></textarea>
+			<textarea id="sequence" class="resizable" rows="25" cols="100"><?php echo "$output"; ?></textarea>
 			</div>
 			</fieldset>
-
+			<form action="MAILTO:<?php echo $to; ?>" method="post" enctype="text/plain">
+			Result:<br>
+			<input type="text" name="name" value="<?php echo $output; ?>"><br>
+			<input type="submit" value="Send">
+			<input type="reset" value="Reset">
 			</form>
 		</div>
 </div>
-
+<p>
+Please kindly cite the following paper if you use Musite or Musite.net in your study: <br/>
+Jianjiong Gao, Jay J. Thelen, A. Keith Dunker, and Dong Xu. Musite: 
+a Tool for Global Prediction of General and Kinase-Specific Phosphorylation Sites.
+<i>Molecular & Cellular Proteomics</i>. 2010. 9(12):2586-600.	<br/>
+<a href="http://www.ncbi.nlm.nih.gov/pubmed/20702892" target="_blank">PubMed Abstract</a>
+&nbsp; &nbsp;<a href="http://www.mcponline.org/content/9/12/2586.abstract" target="_blank">MCP Abstract</a>
+&nbsp; &nbsp;<a href="http://www.mcponline.org/content/9/12/2586.long" target="_blank">MCP Full Text</a>
+</p>
 
 </body>
 </html>
